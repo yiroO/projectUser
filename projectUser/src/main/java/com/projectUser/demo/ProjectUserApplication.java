da0aebdab653f07162992f2dcb1ee1b574eb7e4d
yiroo <yiroo@LAPTOP-08UM5AC3>
2020-06-11 17:53:18 -0500

    Se creo la configuracion en el aplication properties

---
+++ projectUser/src/main/java/com/projectUser/demo/ProjectUserApplication.java
@@ -0,0 +1,13 @@
+package com.projectUser.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUserApplication.class, args);
+	}
+
+}
---
+++ projectUser/src/test/java/com/projectUser/demo/ProjectUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectUser.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
