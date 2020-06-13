ddaa49b582d07c38b674e43d59e823fa95a33c5f
yiroo <yiroo@LAPTOP-08UM5AC3>
2020-06-13 00:00:10 -0500

    Se creo el servicio y controlador para mostrar los datos  en la tabla , roles y para guardar usuarios con roles

--- projectUser/src/main/java/com/projectUser/demo/controller/UserController.java
+++ projectUser/src/main/java/com/projectUser/demo/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.projectUser.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projectUser.demo.emtity.User;
+import com.projectUser.demo.repository.RoleRepository;
+import com.projectUser.demo.service.UserService;
+
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserService userservice;
+	
+	@Autowired
+	private RoleRepository rolrepository;
 
 	@GetMapping({"/", "login"})
 	public String login() {
@@ -13,7 +25,13 @@ public class UserController {
 	}
 	
 	@GetMapping("/userform")
-	public String formUser() {
+	public String formUser(Model model) {
+		
+		model.addAttribute("userForm", new User());
+		model.addAttribute("userList", userservice.findAll());
+		model.addAttribute("roles", rolrepository.findAll());
+		model.addAttribute("listTab", "active");	
+		
 		return "user-form/user-view";
 	}
 }
